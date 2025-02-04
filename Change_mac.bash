49e106adcb300d430e706d838a29ef3790498139
JESTIN <100009504+jestin-k-sunil@users.noreply.github.com>
2025-02-04 11:07:51 +0530

    Update and rename Change_mac to Change_mac.bash

--- Change_mac.bash
+++ Change_mac.bash
@@ -1,11 +1,13 @@
 # MAC Address Spoofing
 
 ## 🖥️ Linux Technique
-```bash
+````bash
 # Check current MAC
 ifconfig or ip link show
-
+````
 # Temporarily change MAC
+````bash
 sudo ifconfig eth0 down
 sudo ifconfig eth0 hw ether 00:11:22:33:44:55
 sudo ifconfig eth0 up
+````
